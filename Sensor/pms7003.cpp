db908675b79eef3f34e74f7c8e40064828caaeb2
psy1064 <psy1064@naver.com>
2019-08-06 02:43:09 -0900

    pms7003 final

--- Sensor/pms7003.cpp
+++ Sensor/pms7003.cpp
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include <wiringPi.h>
 #include <wiringSerial.h>
 
 using namespace std;
@@ -12,7 +13,7 @@ int main()
 {
 	int fd;
 	uint8_t pms[32];
-	uint16_t dust, checkcode = 0;
+	uint16_t pm1, pm25, pm10, checkcode = 0;
 	if ((fd = serialOpen("/dev/ttyUSB0",9600)) < 0)
 	{
 		fprintf(stderr, "Unable serial", strerror (errno));
@@ -21,6 +22,7 @@ int main()
 
 	while(1)
 	{
+		cout << "============================" << endl;
 		for(int i = 0 ; i < 32; i++)
 		{
 			pms[i] = serialGetchar(fd);
@@ -29,15 +31,22 @@ int main()
 				checkcode += pms[i];
 		}
 		cout << endl;
-		if( (pms[0] == 0x42) && (pms[1] == 0x4d) )
+		if ((pms[0] == 0x42) && (pms[1] == 0x4d))
 		{
 			uint16_t tmp = (pms[30] << 8) + pms[31];
-			cout << checkcode << tmp << endl;
-			if(checkcode == tmp)
-				cout << "RIGHT" << endl;
-			dust = (pms[14] << 8) + pms[15];
-			cout << "dust = " << dust << endl;
+			cout << "Check code = " << checkcode << " sum result = " << tmp << endl;
+			if(checkcode == tmp)	cout << "RIGHT" << endl;
+
+			pm1 = (pms[10] << 8) + pms[11];
+			pm25 = (pms[12] << 8) + pms[13];
+			pm10 = (pms[14] << 8) + pms[15];
+
+			cout << "PM 1.0 dust = " << pm1 << endl;
+			cout << "PM 2.5 dust = " << pm25 << endl;
+			cout << "PM 10 dust = " << pm10 << endl;
 		}
 		checkcode = 0;
+		cout << "============================" << endl;
+		delay(1000);
 	}
 }
